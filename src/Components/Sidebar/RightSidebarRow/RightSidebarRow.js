738634b71df4518e3cac98aa4be4c0c689c37d30
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2020-12-10 14:37:36 +0700

    modify ui of main page

--- src/Components/Sidebar/RightSidebarRow/RightSidebarRow.js
+++ src/Components/Sidebar/RightSidebarRow/RightSidebarRow.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./RightSidebarRow.css";
 import { Avatar } from "@material-ui/core";
 
-function RightSidebarRow({ imageUrl, title }) {
+function RightSidebarRow({ userId, imageUrl, title }) {
   return (
-    <div className="right__sidebarRow">
-      <Avatar className="avatar" src={imageUrl} alt={title} />
-      <h1>{title}</h1>
-    </div>
+    <a href={`/${title}/${userId}`}>
+      <div className="right__sidebarRow">
+        <Avatar className="avatar" src={imageUrl} alt={title} />
+        <h1>{title}</h1>
+      </div>
+    </a>
   );
 }
 
